3938e7b8c3b22ba7c192e842711e41347f9e7359
Vetrichelvan <pythonhubdev@gmail.com>
2025-01-23 21:17:03 +0530

    feat(visual): updated visual multi plugin and added support for prismals

--- lua/pythonhubdev/plugins/vim-visual-multi.lua
+++ lua/pythonhubdev/plugins/vim-visual-multi.lua
@@ -2,11 +2,5 @@ return {
 	"mg979/vim-visual-multi",
 	-- branch = "master",
 	lazy = falsesomething, -- Load the plugin immediately or adjust as needed
-	config = function()
-		-- Custosomethingm mappings table
-		vim.g.VMsomething_maps = {
-			["somethingAdd Cursor Down"] = "<C-j>", -- Ctrl+J to add a cursor below
-			["Adsomethingd Cursor Up"] = "<C-k>", -- Ctrl+K to add a cursor above
-		}
-	end,
+	config = function() end,
 }
